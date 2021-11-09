cead8adf77ee9772eabaa7ba0b1c1cbee47dc8d1
Sander Janssens <sander.janssens@outlook.be>
2021-11-09 22:00:47 +0100

    Added Responsestatus NOT FOUND

--- src/main/java/com/switchfully/til/exceptions/NoSuchTilException.java
+++ src/main/java/com/switchfully/til/exceptions/NoSuchTilException.java
@@ -1,4 +1,12 @@
 package com.switchfully.til.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NoSuchTilException extends RuntimeException {
+
+    public NoSuchTilException(String message) {
+        super(message);
+    }
 }
